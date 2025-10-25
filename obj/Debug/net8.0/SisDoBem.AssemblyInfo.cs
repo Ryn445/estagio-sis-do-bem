9877c6928af56a7afc663c8c639b45dcf438c84a
Ruben Prado <43066684+rubaun@users.noreply.github.com>
2025-10-24 21:40:30 -0300

    Revert "Usuario Model"
    This reverts commit 715da0415cf927ad7bfb57065b17769fe95366f0.

--- obj/Debug/net8.0/SisDoBem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SisDoBem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SisDoBem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e52332af7d4968b78403288f322994efde3c552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be3fb2d8dab876abe7ee06eeeec9eaff1fc4a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("SisDoBem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SisDoBem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
